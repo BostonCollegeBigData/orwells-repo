303ff31d65fd891f9c51b64c2770d9f06130bd22
bcbigdata <bcbigdata@gmail.com>
2016-10-18 20:44:01 -0400

    Add files via upload

--- inclass10.18.R
+++ inclass10.18.R
@@ -5,5 +5,15 @@ fit <- lm(uptake ~ conc, data = CO2)
 summary(fit)
 str(fit)
 fitted(fit)
-residuals(fit)
+resid <-residuals(fit)
+hist(resid, col="orange", "border"="pink")
 
+plot(CO2$conc, CO2$uptake, main = "Conc by Uptake")
+#add the line of best fit
+abline(fit)
+
+fit4 <- lm(uptake ~ conc + I(conc**0.3)+ Treatment*Type,
+           data=CO2)
+summary(fit4)
+
+plot(fit4)
